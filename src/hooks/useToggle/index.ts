49e8d356bb08fc8d20c339d66cb1f1f1c2f8451c
CiroLee <ciromail@126.com>
2022-10-22 01:46:06 +0800

    feat: add error tip for useToggle

--- src/hooks/useToggle/index.ts
+++ src/hooks/useToggle/index.ts
@@ -1,8 +1,16 @@
-import { computed, Ref, ref, UnwrapRef } from 'vue';
+import { isRef, Ref, ref, UnwrapRef } from 'vue';
 
 type ToggleFunc = () => void;
 
+/**
+ * @desc hook - toogle a value
+ * @param initValue [optional] initial value
+ * @param reverseValue [optional] reverse value
+ */
 function useToggle<T>(initValue?: T, reverseValue?: T): [Ref<UnwrapRef<T>>, ToggleFunc] {
+  if (isRef(initValue) || isRef(reverseValue)) {
+    throw new Error(`neither initValue nor reverseValue should be typeof Ref`);
+  }
   const state = ref<T>(initValue as T);
   const _reverseValue = reverseValue === undefined ? !state.value : reverseValue;
 
